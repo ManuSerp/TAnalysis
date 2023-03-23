bf1f9d1e575911889912083cead3f7d01c4358fa
Serp <manu.serpette74@gmail.com>
2023-03-23 15:44:21 -0400

    algo ok

--- src/teint.py
+++ src/teint.py
@@ -14,22 +14,30 @@ class TAnalyzer:
         print(self.json)
 
     def gen_def(self, node):
-        if node in self.json["sources"]:
-            return set({node})
-        else:
-            parent = self.ast.get_parents(node)[0]
-            if self.cfg.get_type(parent) == "BinOP":
-                arg = self.cfg.get_var_op(parent)
-                def_tainted = []
+
+        if node in self.json["defs"]:
+            parent = self.cfg.get_parents(node)
+            children = self.cfg.get_children(node)
+            rhs = self.cfg.get_op_hands(children[0])[1]
+            if rhs in self.json["sources"]:
+                return set([node])
+            elif len(parent) > 0:
+                parent = parent[0]
+                if self.cfg.get_type(parent) == "BinOP":
+                    arg = self.get_var_op(parent)
+                elif self.cfg.get_type(parent) == "Variable":
+                    arg = [parent]
+                else:
+                    return set()
+
                 for a in arg:
                     for p in self.json["pairs"]:
                         if a == p[1]:
                             defi = p[0]
                             if defi in OUT[node-self.index]:
-                                def_tainted.add(defi)
-                return set(def_tainted)
-            else:
-                return set()
+                                return set([node])
+
+        return set()
 
     def kill_def(self, node):
         res = []
@@ -44,24 +52,25 @@ class TAnalyzer:
         res = []
         if self.cfg.get_type(node) == "BinOP":
             hands = self.cfg.get_op_hands(node)
-            if self.cfg.get_type(hands[0]) == "Variable":
+            if self.cfg.get_type(hands[0]) == "Variable" or self.cfg.get_type(hands[0]) == "ArrayExpression":
                 res.append(hands[0])
             elif self.cfg.get_type(hands[0]) == "BinOP":
                 res.extend(self.get_var_op(hands[0]))
-            elif self.cfg.get_type(hands[1]) == "Variable":
+            elif self.cfg.get_type(hands[1]) == "Variable" or self.cfg.get_type(hands[1]) == "ArrayExpression":
                 res.append(hands[1])
             elif self.cfg.get_type(hands[1]) == "BinOP":
                 res.extend(self.get_var_op(hands[1]))
         return res
 
-    def poss_t_def(self, cfg: CFG, json):
+    def poss_t_def(self, cfg: CFG):
         self.cfg = cfg
-        self.nodeset = self.cfg.get_nodes()
+        self.nodeset = self.cfg.get_node_ids()
         self.index = self.cfg.get_root()
         IN = [set() for i in range(len(self.nodeset))]
         OUT = [set() for i in range(len(self.nodeset))]
         old_out = [set() for i in range(len(self.nodeset))]
-
+        GEN = [self.gen_def(node) for node in self.nodeset]
+        KILL = [self.kill_def(node) for node in self.nodeset]
         changes = True
         while changes:
             changes = False
@@ -73,8 +82,8 @@ class TAnalyzer:
                     uni = uni.union(OUT[predindex])
                     IN[nodeindex] = uni
                     old_out[nodeindex] = OUT[nodeindex]
-                    OUT[nodeindex] = self.gen_def(nodeindex).union(
-                        IN[nodeindex] - self.kill_def(nodeindex))
+                    OUT[nodeindex] = GEN[nodeindex].union(
+                        IN[nodeindex] - KILL[nodeindex])
                     if OUT[nodeindex] != old_out[nodeindex]:
                         changes = True
         return OUT
@@ -85,4 +94,7 @@ if __name__ == "__main__":
     cfgreader = CFGReader()
     astreader = ASTReader()
     t_analyzer = TAnalyzer()
+    cfg = cfgreader.read_cfg("../tp/part_1/file_1.php.cfg.json")
     t_analyzer.load_json("../tp/part_1/file_1.php.taint.json")
+    out = t_analyzer.poss_t_def(cfg)
+    print(out)
